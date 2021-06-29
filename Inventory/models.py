f7317bc43cdcb547d39f07083d79b824fdf318b3
OsamaAbuHamdan <ohamdan@atypon.com>
2021-06-30 01:14:05 +0300

    Set assignee and team to IT on delete

--- Inventory/models.py
+++ Inventory/models.py
@@ -31,6 +31,14 @@ class Employee(models.Model):
         return self.ldap
 
 
+def get_it_team():
+    return Team.objects.get_or_create(name__iexact='it')
+
+
+def get_it_employee():
+    return Employee.objects.get_or_create(ldap__iexact='it')
+
+
 class Item(models.Model):
     asset_tag = models.IntegerField(primary_key=True)
     location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
@@ -39,8 +47,8 @@ class Item(models.Model):
     model = models.CharField(max_length=100)
     service_tag = models.CharField(max_length=100)
     system_name = models.CharField(max_length=100, blank=True, null=True)
-    assigned_to = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True)
-    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True)
+    assigned_to = models.ForeignKey(Employee, on_delete=models.SET(get_it_employee), null=True)
+    team = models.ForeignKey(Team, on_delete=models.SET(get_it_team), null=True)
     acquisition_date = models.DateField(default=datetime.date.today)
     decommission_date = models.DateField(blank=True, null=True)
     notes = models.CharField(max_length=256, blank=True, null=True)
